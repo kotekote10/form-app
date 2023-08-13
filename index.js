fab3c5f3211b514fc3deff43b387da3149cc2daa
kote spirandi <konstantine.spirandi@gmail.com>
2023-08-13 17:51:04 +0400

    uibuild

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const formData = [];
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static("build"));
+
+app.get("/api/data", (request, response) => {
+  console.log(formData);
+  response.json(formData);
+});
+
+app.post("/api/data", (request, response) => {
+  const data = request.body;
+  const loan = (Number(data.amount) * 125) / 100;
+  const monthlyPay = loan / data.months;
+  const additionalIncome = data.additional
+    .map((x) => Number(x.income))
+    .reduce((partialSum, a) => partialSum + a, 0);
+  const wholeIncome = additionalIncome + Number(data.salary);
+  const abilityToPay = (wholeIncome * 40) / 100;
+  const toSend = {
+    monthlyPay: monthlyPay,
+    loan: loan,
+    percentage: 25,
+    excess: (loan * 20) / 100,
+    paydate: data.paydate,
+  };
+  if (!data) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  } else {
+    if (monthlyPay > abilityToPay) {
+      formData.unshift("rejected");
+    } else {
+      formData.unshift(toSend);
+    }
+    console.log(data);
+    response.json(formData);
+  }
+});
+console.log(formData);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
